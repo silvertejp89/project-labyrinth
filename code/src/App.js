67b6089c1472b2986eae56ad82597fb38cd25f18
Kriss Stockhaus <80514864+silvertejp89@users.noreply.github.com>
2024-01-22 19:07:14 +0100

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
@@ -12,6 +12,20 @@ const reducer = combineReducers({
 const store = configureStore({ reducer });
 
 export const App = () => {
+    //-----temporary alert------------------
+   useEffect(() => {
+    const handleLoad = () => {
+      alert('🛠️ Database Offline: the website is currently undergoing maintenance, but feel free to check out the code on github in the meantime!');
+    };
+
+    window.onload = handleLoad;
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.onload = null;
+    };
+  }, []);
+  //------------------------------------------
   return (
     <Provider store={store}>
       <Game />
